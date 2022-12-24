785e618483882b47a74e4eb369c1294496643f92
amator82 <ilyakorckishko@gmail.com>
2022-12-24 21:17:30 +0200

    add sidiber content

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,8 +1,51 @@
 import React from 'react'
 import './_sidebar.scss'
 
-const Sidebar = () => {
-    return <div className='border border-danger'>Sidebar</div>
+import {
+    MdSubscriptions,
+    MdExitToApp,
+    MdThumbUp,
+    MdHistory,
+    MdLibraryBooks,
+    MdHome,
+    MdSentimentDissatisfied
+} from 'react-icons/md'
+
+const Sidebar = ({ openSidebar }) => {
+    return (
+        <aside className={openSidebar ? 'sidebar open' : 'sidebar'}>
+            <ul className='sidebar__list'>
+                <li className='sidebar__link'>
+                    <MdHome size={23} />
+                    <span>Home</span>
+                </li>
+                <li className='sidebar__link'>
+                    <MdSubscriptions size={23} />
+                    <span>Subscriptions</span>
+                </li>
+                <li className='sidebar__link'>
+                    <MdThumbUp size={23} />
+                    <span>Liked Video</span>
+                </li>
+                <li className='sidebar__link'>
+                    <MdHistory size={23} />
+                    <span>History</span>
+                </li>
+                <li className='sidebar__link'>
+                    <MdLibraryBooks size={23} />
+                    <span>Library</span>
+                </li>
+                <li className='sidebar__link'>
+                    <MdSentimentDissatisfied size={23} />
+                    <span>bruh</span>
+                </li>
+                <li className='sidebar__link sidebar__link_border'>
+                    <MdExitToApp size={23} />
+                    <span>Log out</span>
+                </li>
+            </ul>
+        </aside>
+    )
 }
 
 export default Sidebar
