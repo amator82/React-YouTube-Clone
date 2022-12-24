f5d8429b35df22421c17eed70985e191317a8685
amator82 <ilyakorckishko@gmail.com>
2022-12-24 18:32:13 +0200

    add basic structure Home page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,23 @@
-function App() {}
+import React from 'react'
+
+import { Container } from 'react-bootstrap'
+
+import Header from './components/header/Header'
+import Sidebar from './components/sidebar/Sidebar'
+import HomeScreen from './screens/homeScreen/HomeScreen'
+
+const App = () => {
+    return (
+        <>
+            <Header />
+            <div className='app__container border border-info'>
+                <Sidebar />
+                <Container fluid className='app__main border border-warning'>
+                    <HomeScreen />
+                </Container>
+            </div>
+        </>
+    )
+}
 
 export default App
