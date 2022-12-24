fe4bf3258827b4d9b1510dbc17f0c411533e13db
amator82 <ilyakorckishko@gmail.com>
2022-12-24 19:28:06 +0200

    change header logo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './_header.scss'
 
-import { FaBars } from 'react-icons/fa'
+import { FaBars, FaYoutube } from 'react-icons/fa'
 import { AiOutlineSearch } from 'react-icons/ai'
 import { MdNotifications, MdApps } from 'react-icons/md'
 
@@ -9,11 +9,7 @@ const Header = () => {
     return (
         <div className='header border border-dark'>
             <FaBars className='header__menu' size={26} />
-            <img
-                src='https://www.logo.wine/a/logo/YouTube/YouTube-Icon-Full-Color-Logo.wine.svg'
-                alt='logo'
-                className='header__logo'
-            />
+            <FaYoutube size={30} className='header__logo' />
             <form>
                 <input type='text' placeholder='Search' />
                 <button type='submit'>
@@ -26,6 +22,7 @@ const Header = () => {
                 <img
                     src='https://cdn.icon-icons.com/icons2/2643/PNG/512/male_boy_person_people_avatar_icon_159358.png'
                     alt='avatar'
+                    className='header__avatar'
                 />
             </div>
         </div>
