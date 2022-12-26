d639297d4b3692fd79f9032ffaad9883c37992ec
amator82 <ilyakorckishko@gmail.com>
2022-12-26 15:19:42 +0200

    create Login Screen

---
+++ src/screens/loginScreen/LoginScreen.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './loginScreen.scss'
+
+const LoginScreen = () => {
+    return (
+        <div className='login'>
+            <div className='login__container'>
+                <div className='login__image'>
+                    <img
+                        src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/YouTube_full-color_icon_%282017%29.svg/800px-YouTube_full-color_icon_%282017%29.svg.png'
+                        alt='logo'
+                    />
+                </div>
+                <button className='login__button'>Login with Google</button>
+                <p className='login__text'>Made by using YouYube Data API</p>
+            </div>
+        </div>
+    )
+}
+
+export default LoginScreen
