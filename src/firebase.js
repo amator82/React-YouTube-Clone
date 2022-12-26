f1bf2fd570c9183a8951df9ee4229256cdc9a73a
amator82 <ilyakorckishko@gmail.com>
2022-12-26 23:31:48 +0200

    install firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyBG0FWjHogmSm53s5la9KTGqh5t637eApA',
+    authDomain: 'ytcloneam82.firebaseapp.com',
+    projectId: 'ytcloneam82',
+    storageBucket: 'ytcloneam82.appspot.com',
+    messagingSenderId: '359388639067',
+    appId: '1:359388639067:web:1fb9c3d76a83e3a240747e'
+}
+
+firebase.initializeApp(firebaseConfig)
