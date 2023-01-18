a5c2be95638c118eefdf27a5bfc35b0e66eac96f
amator82 <ilyakorckishko@gmail.com>
2023-01-18 11:42:32 +0200

    change home page skeleton

--- src/components/video/VideoSkeleton.jsx
+++ src/components/video/VideoSkeleton.jsx
@@ -4,17 +4,20 @@ import ContentLoader from 'react-content-loader'
 const VideoSkeleton = () => (
     <ContentLoader
         speed={2}
-        width='320'
-        height='272'
-        viewBox='0 0 310 272'
+        width={312}
+        height={280}
+        viewBox='0 0 312 280'
         backgroundColor='#f3f3f3'
         foregroundColor='#ecebeb'
     >
-        <rect x='0' y='0' rx='20' ry='20' width='309' height='172' />
-        <rect x='0' y='180' rx='3' ry='3' width='306' height='6' />
-        <rect x='0' y='193' rx='3' ry='3' width='164' height='6' />
-        <circle cx='24' cy='230' r='24' />
-        <rect x='55' y='225' rx='3' ry='3' width='92' height='6' />
+        <circle cx='18' cy='258' r='18' />
+        <rect x='337' y='0' rx='5' ry='5' width='150' height='23' />
+        <rect x='337' y='35' rx='5' ry='5' width='722' height='48' />
+        <rect x='337' y='100' rx='5' ry='5' width='80' height='20' />
+        <rect x='0' y='0' rx='20' ry='20' width='312' height='175' />
+        <rect x='0' y='185' rx='7' ry='7' width='312' height='21' />
+        <rect x='0' y='215' rx='4' ry='4' width='312' height='16' />
+        <rect x='49' y='252' rx='5' ry='5' width='90' height='12' />
     </ContentLoader>
 )
 export default VideoSkeleton
